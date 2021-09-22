7d610172bc81455af457914ff793cab67d3804d5
unknown <andredejesus18@hotmail.com>
2021-09-22 00:27:24 -0300

    Projeto Completo

---
+++ ProdutoManager/src/main/java/net/codejava/Product.java
@@ -0,0 +1,70 @@
+package net.codejava;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	
+	private long id;
+	private String name;
+	private String marca;
+	private String madein;
+	private float valor;
+	
+	protected Product() {
+		 
+		
+		
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getMadein() {
+		return madein;
+	}
+
+	public void setMadein(String madein) {
+		this.madein = madein;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
+	
+	
+	
+	
+
+}
